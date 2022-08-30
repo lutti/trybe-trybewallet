f994485650249a508549db85832c0d4268a5faa3
Lutti <lutti.c.amaral@gmail.com>
2022-08-29 21:17:04 -0300

    Requisito 1 quase

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,80 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      email: '',
+      password: '',
+      enterDisabled: true,
+      loginDone: false,
+    };
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+      enterDisabled: this.isButtonDisabled(),
+    });
+  }
+
+  isValidEmail = (email) => /\S+@\S+\.\S+/.test(email);
+
+  isButtonDisabled = () => {
+    const { email, password } = this.state;
+    const minPassLenght = 4;
+
+    return !(this.isValidEmail(email) && password.length > minPassLenght);
+  };
+
+  loginEvent = () => {
+    this.setState({
+      loginDone: true,
+    });
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password, loginDone, enterDisabled } = this.state;
+    if (loginDone) {
+      return <Redirect push to="/carteira" />;
+    }
+    return (
+      <div>
+        <label htmlFor="email">
+          Email:
+          <input
+            type="email"
+            id="email"
+            name="email"
+            data-testid="email-input"
+            onChange={ this.handleChange }
+            value={ email }
+          />
+        </label>
+        <label htmlFor="password">
+          Password:
+          <input
+            type="password"
+            id="password"
+            name="password"
+            data-testid="password-input"
+            onChange={ this.handleChange }
+            value={ password }
+          />
+        </label>
+        <input
+          type="button"
+          value="Entrar"
+          onClick={ this.loginEvent }
+          disabled={ enterDisabled }
+        />
+      </div>
+    );
   }
 }
 
