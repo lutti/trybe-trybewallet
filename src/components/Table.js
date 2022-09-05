ce83c1d2f0822bc0556115a2df040845b85260f2
Lutti <lutti.c.amaral@gmail.com>
2022-09-05 17:27:51 -0300

    Requisito 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 
 class Table extends Component {
+  // Descrição, Tag, Método de pagamento,Valor, Moeda, Câmbio utilizado, Valor convertido, Moeda de conversão e Editar/Excluir
   render() {
     return (
-      <div>Table</div>
+      <div>
+        Table
+        <table>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+          <tr>
+            {/* <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td> */}
+          </tr>
+        </table>
+      </div>
     );
   }
 }
